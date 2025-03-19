6ad54ecf651155cf643293a5c6161635c2681d65
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-03-19 16:41:19 +0800

    Update telegram_reader.py

--- utils/telegram_reader.py
+++ utils/telegram_reader.py
@@ -1,9 +1,13 @@
 from telethon import TelegramClient
 
+def extract_channel_username(url):
+    return '@' + url.strip().rstrip('/').split('/')[-1]
+
 async def fetch_latest_messages(api_id, api_hash, channel_username, limit=5):
     client = TelegramClient("session", api_id, api_hash)
     await client.start()
     messages = []
+
     async for message in client.iter_messages(channel_username, limit=limit):
         if message.text:
             messages.append({
@@ -11,5 +15,6 @@ async def fetch_latest_messages(api_id, api_hash, channel_username, limit=5):
                 "text": message.text,
                 "date": str(message.date)
             })
+
     await client.disconnect()
     return messages
